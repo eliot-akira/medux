8c6ac913b57345cb5cf6ab70f5a0c3aaf4601d25
eliot-akira <me@eliotakira.com>
2020-05-31 14:32:02 +0200

    Update tests

--- src/test.js
+++ src/test.js
@@ -1,5 +1,5 @@
 import { test, runTests } from 'testra'
-import createStore from './index'
+import { createStore } from './index'
 
 // Tests
 
@@ -32,29 +32,29 @@ test('store.setState', it => {
   const oldState = { index: 0, sameObj }
   const newState = store.setState(oldState)
 
-  it('updates state', store.state.index===0)
+  it('updates state', store.state.index===0, store.state, newState)
   it('creates new state on update', oldState!==newState)
   it('preserves unchaged objects', store.state.sameObj===sameObj)
 })
 
-test('store[action]', it => {
+test('store.actions[action]', it => {
   const oldState = { index: 0 }
   const store = createStore({
     state: oldState,
     actions: {
-      increment(store) {
-        store.setState({ index: store.state.index+1 })
+      increment() {
+        this.setState({ index: store.state.index+1 })
       }
     }
   })
 
-  store.increment()
+  store.actions.increment()
   it('updates state', store.state.index===1)
   it('does not mutate state', store.state!==oldState)
   it('does not mutate state key', store.state.index!==oldState.index)
 })
 
-test('on(\'setState\')', it => {
+test('on(\'state\')', it => {
 
   let called = false
   let calledWithValue = 0
@@ -63,9 +63,9 @@ test('on(\'setState\')', it => {
     state: { index: 0 }
   })
 
-  store.on('setState', (store) => {
+  store.on('state', (state) => {
     called = true
-    calledWithValue = store.state.index
+    calledWithValue = state.index
   })
 
   store.setState({ index: 1 })
@@ -83,61 +83,64 @@ test('on(\'action\')', it => {
   const store = createStore({
     state: { index: 0 },
     actions: {
-      increment: (store, props) => store.setState(draft => {
-        draft.index += props
-      })
+      increment(props) {
+        this.setState(draft => {
+          draft.index += props
+        })
+      }
     },
   })
 
-  store.on('action', (store, key, props) => {
+  store.on('action', (key, props) => {
     called = true
     calledWithKey = key
     calledWithProps = props
   })
 
-  store.increment(2)
+  store.actions.increment(2)
 
   it('is called on action', called)
   it('is called on action with key', calledWithKey==='increment')
   it('is called on action with props', calledWithProps===2)
 })
 
-test('store[childStore]', it => {
+test('child state and actions', it => {
 
   let called = false
   let childCalled = false
   const storeProps = {
-    state: { index: 0 },
+    state: {
+      index: 0,
+      child: {
+        index: 0
+      }
+    },
     actions: {
-      increment(store) {
-        store.setState({ index: store.state.index + 1 })
+      increment() {
+        this.setState({ index: this.state.index + 1 })
+      },
+      child: {
+        increment() {
+          this.setState({ index: this.state.index + 1 })
+          childCalled = true
+        }
       }
     }
   }
-  const store = createStore({
-    ...storeProps,
-    stores: {
-      child: storeProps
-    },
-  })
 
-  store.on('setState', (store) => {
+  const store = createStore(storeProps)
+
+  store.on('state', (state) => {
     called = true
   })
 
-  store.child.on('setState', (store) => {
-    childCalled = true
-  })
-
-  it('is created', store.child)
-  it('has state', store.child.state)
-  it('has actions', store.child.increment)
+  it('has child state', store.state.child)
+  it('has child actions', store.actions.child.increment)
   it('parent store state has child state', store.state.child)
-  it('parent and child stores have same child state reference', store.state.child===store.child.state)
 
   const oldParentState = store.state
 
-  store.child.increment()
+  store.actions.child.increment()
 
   const newParentState = store.state
 
@@ -145,8 +148,7 @@ test('store[childStore]', it => {
   it('action updates parent state', store.state.child.index===1)
   it('action does not affect unrelated parent state keys', store.state.index===0)
 
-  it('action updates child store state', store.child.state.index===1)
-  it('state update after action is shared by parent and child store', store.state.child===store.child.state)
+  it('action updates child store state', store.state.child.index===1)
 
   it('action calls child setState', childCalled)
   it('action calls parent setState', called)
